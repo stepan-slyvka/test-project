62f9e90dde28b2702adbf553dd947910f4cbb2ae
Stepan <skysword2014@gmail.com>
2022-07-25 10:34:25 +0300

    Fixed adding an invoice. Page does not crash anymore when viewing the newly created invoice

--- src/components/Pages/Invoice/InvoiceItem.js
+++ src/components/Pages/Invoice/InvoiceItem.js
@@ -1,21 +1,18 @@
 import React from "react";
-
 import { useParams } from "react-router-dom";
-
+import { useSelector } from "react-redux";
 import InvoiceItemDescription from "../Invoice/InvoiceItemDescription";
-
-import { INVOICES_LIST } from "./InvoicesList";
-
 import classes from "../Invoice/InvoiceItem.module.css";
 
 const InvoiceItem = () => {
   const { invoiceId } = useParams();
+  const invoices = useSelector((state) => state.invoice.invoices);
 
-  const invoice = INVOICES_LIST.find((invoice) => invoice.id === invoiceId);
+  const invoice = invoices.find((invoice) => invoice.id === invoiceId);
 
   return invoice ? (
     <InvoiceItemDescription
-      invoice_num={invoice.invoice_num}
+      invoiceNumber={invoice.invoice_num}
       status={invoice.status}
       order_date={invoice.order_date}
       bill_from={invoice.bill_from}
